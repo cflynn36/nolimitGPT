cda2935516e6f1972981e78c5bdbecfd223d280e
conan <conan@debcon.customer.ask4.lan>
2023-04-22 13:20:39 +0100

    changed Google Authorised Redirect URL

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.set('view engine', 'ejs');
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/auth/google/callback"
+    callbackURL: "https://rocky-falls-15188.herokuapp.com/auth/google/callback"
   },
   async (accessToken, refreshToken, profile, done) => {
     const email = profile.emails[0].value;
